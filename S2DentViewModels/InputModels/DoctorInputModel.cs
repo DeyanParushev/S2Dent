d81ef8acd2e352566604c665a7ee46c5ac0aef5b
Deyan Parushev <deqnparushev@gmail.com>
2021-02-05 07:48:25 +0200

    Work on data anotations validation.

--- S2DentViewModels/InputModels/DoctorInputModel.cs
+++ S2DentViewModels/InputModels/DoctorInputModel.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-   
+    using System.Resources;
     using Microsoft.AspNetCore.Http;
     
     using S2Dent.Models;
@@ -10,31 +10,31 @@
 
     public class DoctorInputModel : IMapFrom<Doctor>, IMapTo<Doctor>
     {
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(String))]
+        [Required]
+        //[StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(ResourceSet))]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
+        //[StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(ResourceSet))]
         public string MiddleName { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
+        //[StringLength(20, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(ResourceSet))]
         public string LastName { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [StringLength(100, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
+        //[StringLength(100, ErrorMessageResourceName = "NameIsTooLong", ErrorMessageResourceType = typeof(ResourceSet))]
         public string Specialty { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [StringLength(450, ErrorMessageResourceName = "DescriptionIsTooLong", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
+        //[StringLength(450, ErrorMessageResourceName = "DescriptionIsTooLong", ErrorMessageResourceType = typeof(ResourceSet))]
         public string Description { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(String))]
-        [EmailAddress(ErrorMessageResourceName = "InvalidEmail", ErrorMessageResourceType = typeof(String))]
+        //[Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ResourceSet))]
+        //[EmailAddress(ErrorMessageResourceName = "InvalidEmail", ErrorMessageResourceType = typeof(ResourceSet))]
         public string Email { get; set; }
 
         public IFormFile ProfilePicture { get; set; }
